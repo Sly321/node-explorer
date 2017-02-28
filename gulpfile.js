a95e2e21ffa0c1a80c79bcc6e4d0fad3cd2ecf33
Sven Liebig <liebigsv@gmail.com>
2017-02-28 11:44:09 +0100

    added typescript import

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,32 @@
 var gulp = require('gulp');
 var util = require('gulp-util');
+var tsc = require('gulp-typescript');
 var mocha = require('gulp-mocha');
+var minify = require('gulp-minify');
 var runSequence = require('run-sequence');
 
 function handleError(err) {
     console.log(err.toString());
     this.emit('end');
 }
+
+gulp.task('compile-ts', ['compile-src', 'compile-test']);
+
+gulp.task('compile-test', function () {
+    var tsProject = tsc.createProject('tsconfig.json');
+    return gulp.src(['./test/*.ts'])
+        .pipe(tsProject())
+        .on('error', util.log)
+        .pipe(gulp.dest('./test/'));
+});
+
+gulp.task('compile-src', function () {
+    var tsProject = tsc.createProject('tsconfig.json');
+    return gulp.src(['./src/app/*.ts'])
+        .pipe(tsProject())
+        .on('error', util.log)
+        .pipe(gulp.dest('./src/app/'));
+});
 
 gulp.task('test', function () {
     return gulp.src('./test/*.js', { read: false })
@@ -16,11 +36,11 @@ gulp.task('test', function () {
 });
 
 gulp.task('watch', function () {
-    gulp.watch(['./test/*.js', './src/app/*.js'], function () {
-        runSequence('test');
+    gulp.watch(['./test/*.ts', './src/app/*.ts'], function () {
+        runSequence('compile-ts', 'test');
     });
 });
 
 gulp.task('default', function () {
-    runSequence('test', 'watch');
+    runSequence('compile-ts', 'test', 'watch');
 });
